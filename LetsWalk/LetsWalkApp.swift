65e381debf02d4e256aefbe671b83f2373d2ffac
Sabri <sabri.sonmez@macaulay.cuny.edu>
2021-08-24 16:58:17 -0400

    Initial Commit

---
+++ LetsWalk/LetsWalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsWalkApp.swift
+//  LetsWalk
+//
+//  Created by Sabri Sönmez on 8/24/21.
+//
+
+import SwiftUI
+
+@main
+struct LetsWalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
